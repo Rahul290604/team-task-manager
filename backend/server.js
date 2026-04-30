221f842c869abf5287bceffcddfd5dc8cb75a697
Shashank Shekhar <sshkhar21@gmail.com>
2026-04-30 16:32:57 +0530

    final fix

--- backend/server.js
+++ backend/server.js
@@ -6,22 +6,22 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// ✅ THIS MUST EXIST
 app.get("/", (req, res) => {
   res.send("API is running 🚀");
 });
 
-mongoose.connect("mongodb://rahul290604_db_user:Rahul123@ac-s8mjls5-shard-00-00.b0kp2k2.mongodb.net:27017,ac-s8mjls5-shard-00-01.b0kp2k2.mongodb.net:27017,ac-s8mjls5-shard-00-02.b0kp2k2.mongodb.net:27017/?ssl=true&replicaSet=atlas-hidecw-shard-0&authSource=admin&appName=Cluster0")
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+// ✅ ROUTES
 app.use("/auth", require("./routes/auth"));
-app.use("/projects", require("./routes/projects"));
 app.use("/tasks", require("./routes/tasks"));
-const User = require("./models/User");
 
-app.get("/users", async (req, res) => {
-  const users = await User.find();
-  res.json(users);
-});
+// ✅ DB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// ✅ PORT + HOST
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
